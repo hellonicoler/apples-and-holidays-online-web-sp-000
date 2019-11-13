2ce7b2be5523b0c15518b73d7a004b53def6907e
Nicole Rothenay <nthorn1211@gmail.com>
2019-11-13 20:30:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday_with_supplies|
-  
+  puts ""
 end
 
 def all_holidays_with_bbq(holiday_hash)
