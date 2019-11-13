3b2445d459f117638484d757e51a5938c44b3ffc
Nicole Rothenay <nthorn1211@gmail.com>
2019-11-13 20:32:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday_with_supplies|
   puts "#{season.capitalize}:"
   holiday_with_supplies.each do |holiday, supplies|
-    
+    holiday.to_s
 end
 
 def all_holidays_with_bbq(holiday_hash)
